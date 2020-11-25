cc912fb5a6b707075cfd842f33624108dcc79e7b
NGUYỄN THÀNH VĨNH PHÚC <1712675@student.hcmus.edu.vn>
2020-11-25 15:45:00 +0700

    Update giao diện

--- WeSplitApp/View/HomeScreen.xaml.cs
+++ WeSplitApp/View/HomeScreen.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using MahApps.Metro.Controls;
+using MaterialDesignExtensions.Controls;
+using MaterialDesignExtensions.Model;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +16,88 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WeSplitApp.ViewModel;
 
 namespace WeSplitApp.View
 {
     /// <summary>
     /// Interaction logic for HomeScreen.xaml
     /// </summary>
-    public partial class HomeScreen : MetroWindow
+    public partial class HomeScreen : MaterialWindow
     {
+        public const string DialogHostName = "dialogHost";
+
+        public DialogHost DialogHost => m_dialogHost;
+
+        private List<INavigationItem> m_navigationItems;
+        public List<INavigationItem> NavigationItems => m_navigationItems;
+
         public HomeScreen()
         {
+            m_navigationItems = new List<INavigationItem>()
+            {
+                new SubheaderNavigationItem() { Subheader = "Chuyến đi"},
+                new FirstLevelNavigationItem() { Label = "Đã kết thúc", Icon = PackIconKind.Passport, NavigationItemSelectedCallback = item => new HaveTakenTripsListViewViewModel()},
+                new FirstLevelNavigationItem() { Label = "Đang đi/Sắp tới", Icon = PackIconKind.Plane, NavigationItemSelectedCallback = item => new BeingTakenTripsListViewViewModel()},
+                new FirstLevelNavigationItem() { Label = "Thêm mới", Icon = PackIconKind.Add, NavigationItemSelectedCallback = item => new AddNewTripViewModel()},
+                new SubheaderNavigationItem() { Subheader = "Khác"},
+                new FirstLevelNavigationItem() { Label = "Cài đặt", Icon = PackIconKind.Settings, NavigationItemSelectedCallback = item => new SettingsViewModel()},
+                new FirstLevelNavigationItem() { Label = "Về chúng tôi", Icon = PackIconKind.About, NavigationItemSelectedCallback = item => new AboutUsViewModel()},
+            };
+
             InitializeComponent();
+
+            sideNav.DataContext = this;
+            navigationDrawerNav.DataContext = this;
+
+            Loaded += LoadedHandler;
+        }
+
+        private void LoadedHandler(object sender, RoutedEventArgs args)
+        {
+            navigationDrawerNav.SelectedItem = m_navigationItems[1];
+            sideNav.SelectedItem = m_navigationItems[1];
+            m_navigationItems[1].IsSelected = true;
+        }
+
+        private void NavigationItemSelectedHandler(object sender, NavigationItemSelectedEventArgs args)
+        {
+            SelectNavigationItem(args.NavigationItem);
+        }
+
+        private void SelectNavigationItem(INavigationItem navigationItem)
+        {
+            if (navigationItem != null)
+            {
+                object newContent = navigationItem.NavigationItemSelectedCallback(navigationItem);
+
+                if (contentControl.Content == null || contentControl.Content.GetType() != newContent.GetType())
+                {
+                    contentControl.Content = newContent;
+                }
+            }
+            else
+            {
+                contentControl.Content = null;
+            }
+
+            if (appBar != null)
+            {
+                appBar.IsNavigationDrawerOpen = false;
+            }
+        }
+
+        private void GoToGitHubButtonClickHandler(object sender, RoutedEventArgs args) => OpenLink("https://github.com/phucnguyen1712675/WeSplitApp");
+
+        private void OpenLink(string url)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
         }
     }
 }
---
+++ WeSplitApp/ViewModel/HaveTakenTripsListViewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSplitApp.ViewModel
+{
+    public class HaveTakenTripsListViewViewModel : INotifyPropertyChanged
+    {
+#pragma warning disable 67
+        public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore 67
+
+    }
+}
